bccdb798ff4a7b6b94107f6cf9512ab18bb838d0
o.rzazade <o.rzazade@gmail.com>
2020-03-18 12:45:25 +0200

    Updates

---
+++ precache-manifest.9926f044ce1e099e7e31edc51376221f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4807980e94029649aea51eb88a790b8f",
+    "url": "/antd-copyable-link/index.html"
+  },
+  {
+    "revision": "97090330303f385dd17e",
+    "url": "/antd-copyable-link/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "dddd657d5580411d4521",
+    "url": "/antd-copyable-link/static/css/main.50695a3d.chunk.css"
+  },
+  {
+    "revision": "97090330303f385dd17e",
+    "url": "/antd-copyable-link/static/js/2.8f7dc9c3.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/antd-copyable-link/static/js/2.8f7dc9c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dddd657d5580411d4521",
+    "url": "/antd-copyable-link/static/js/main.c5d20e76.chunk.js"
+  },
+  {
+    "revision": "ce22049783398ba69de2",
+    "url": "/antd-copyable-link/static/js/runtime-main.04b37ed2.js"
+  }
+]);
