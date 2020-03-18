f338c7c1e1e8626a8de3042780ccf8ccca7203b3
o.rzazade <o.rzazade@gmail.com>
2020-03-18 13:17:43 +0200

    change ss gif

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './App.css';
 import { CopyableLink } from './copyableLink';
-import { Input } from 'antd';
+import { Input, Row, Col } from 'antd';
 const { TextArea } = Input;
 
 function App() {
   return (
-    <>
-      <CopyableLink
+    <Row style={{paddingTop:"30px"}}>
+    <Col span={12} offset={6}>
+    <CopyableLink
         to="https://google.com"
         copyableText="Hi, I am the copied text"
-        title="I am Tittle" />
+        title="I am a tittle" />
 
       <TextArea rows={4} />
-    </>
+    </Col>
+  </Row>
+     
   );
 }
 
